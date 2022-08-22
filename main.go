a2966c310049ca5d5385fba9c3f46c0ebbafee1f
ghani <agaarbi@gmail.com>
2022-08-22 12:54:54 +0500

    added comments for better understanding

--- main.go
+++ main.go
@@ -6,39 +6,48 @@ import (
 	"fmt"
 )
 
+// creates array of Block Addresses
 type BlockChain struct {
 	blocks []*Block
 }
 
+// timestamp, block height etc are added when system becomes complicated
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
 }
 
-// method
+// method of Block struct
 func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
+	// used [:] to create slice from an array
 	b.Hash = hash[:]
 }
 
+// Block is created on address
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash}
 	block.DeriveHash()
 	return block
 }
 
+// method of BlockChain struct
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.blocks[len(chain.blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
 	chain.blocks = append(chain.blocks, new)
 }
 
+// returns pointer to the Block created by CreateBlock
+// using Genesis as the data and empty previous hash value
+// Genesis means "the origin or mode of formation of something"
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
+// returns pointer addresses to the BlockChain struct
 func InitBlockChain() *BlockChain {
 	return &BlockChain{[]*Block{Genesis()}}
 }
